afd71b51729e94b01da979ef4c4e348ff34c3aa5
Sigurd Midttun <sigurd.midttun@nav.no>
2018-03-07 15:04:59 +0100

    Initial Commit

---
+++ app/src/main/java/no.nav.dokopp/Application.java
@@ -0,0 +1,14 @@
+package no.nav.dokopp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
+
