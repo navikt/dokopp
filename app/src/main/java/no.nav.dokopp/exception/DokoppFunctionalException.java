fe9d4103c995da47cda9246fa49649d774c37a89
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2018-04-03 14:02:43 +0200

    PKANAKONDA-4906 Implementert tjoark122 consumer

--- app/src/main/java/no.nav.dokopp/exception/DokoppFunctionalException.java
+++ app/src/main/java/no.nav.dokopp/exception/DokoppFunctionalException.java
@@ -4,21 +4,12 @@ package no.nav.dokopp.exception;
  * @author Jarl Øystein Samseth, Visma Consulting
  */
 public class DokoppFunctionalException extends RuntimeException {
-	
-	private String shortMessage;
-	
-	public DokoppFunctionalException(String message, String shortMessage) {
+
+	public DokoppFunctionalException(String message) {
 		super(message);
-		this.shortMessage=shortMessage;
 	}
-	
-	public DokoppFunctionalException(String message, String shortMessage, Throwable cause) {
+
+	public DokoppFunctionalException(String message, Throwable cause) {
 		super(message, cause);
-		this.shortMessage=shortMessage;
-	}
-	
-	
-	public String getShortMessage(){
-		return shortMessage;
 	}
 }
