2477441441f610270bfad0a1145f2a956ca077d9
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2018-04-03 15:23:30 +0200

    PKANAKONDA-4906 Implementerte settJournalpostAttributter consumer

---
+++ app/src/main/java/no/nav/dokopp/qopp001/tjoark110/Tjoark110SettJournalpostAttributter.java
@@ -0,0 +1,32 @@
+package no.nav.dokopp.qopp001.tjoark110;
+
+import no.nav.tjeneste.domene.brevogarkiv.arkiverdokumentproduksjon.v1.ArkiverDokumentproduksjonV1;
+import no.nav.tjeneste.domene.brevogarkiv.arkiverdokumentproduksjon.v1.meldinger.SettJournalpostAttributterRequest;
+import org.apache.camel.Handler;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+/**
+ * @author Sigurd Midttun, Visma Consulting.
+ */
+@Service
+public class Tjoark110SettJournalpostAttributter {
+	private final ArkiverDokumentproduksjonV1 arkiverDokumentproduksjonV1;
+
+	@Inject
+	public Tjoark110SettJournalpostAttributter(ArkiverDokumentproduksjonV1 arkiverDokumentproduksjonV1) {
+		this.arkiverDokumentproduksjonV1 = arkiverDokumentproduksjonV1;
+	}
+
+	@Handler
+	public void settJournalpostAttributter(SettJournalpostAttributterRequestTo settJournalpostAttributterRequestTo) {
+		arkiverDokumentproduksjonV1.settJournalpostAttributter(mapRequest(settJournalpostAttributterRequestTo));
+	}
+
+	private SettJournalpostAttributterRequest mapRequest(SettJournalpostAttributterRequestTo settJournalpostAttributterRequestTo) {
+		return new SettJournalpostAttributterRequest()
+				.withJournalpostIdListe(Long.valueOf(settJournalpostAttributterRequestTo.getJournalpostId()))
+				.withAntallReturpost(settJournalpostAttributterRequestTo.getAntallRetur());
+	}
+}
