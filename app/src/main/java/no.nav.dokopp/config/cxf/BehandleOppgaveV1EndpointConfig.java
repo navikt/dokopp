b86d01452667f9f357e5699a6e7c9d25d2291eea
Sigurd Midttun <sigurd.midttun@nav.no>
2018-04-03 11:59:10 +0200

    PKANAKONDA-4906 Oppdaterte behandleOppgaveV1Config

--- app/src/main/java/no.nav.dokopp/config/cxf/BehandleOppgaveV1EndpointConfig.java
+++ app/src/main/java/no.nav.dokopp/config/cxf/BehandleOppgaveV1EndpointConfig.java
@@ -4,6 +4,7 @@ import no.nav.dokopp.config.fasit.BehandleOppgaveV1Alias;
 import no.nav.dokopp.config.fasit.NavAppCertAlias;
 import no.nav.modig.security.ws.SystemSAMLOutInterceptor;
 import no.nav.tjeneste.virksomhet.behandleoppgave.v1.binding.BehandleOppgaveV1;
+import org.apache.cxf.ws.addressing.WSAddressingFeature;
 import org.springframework.context.annotation.Bean;
 
 import javax.xml.namespace.QName;
@@ -18,7 +19,7 @@ public class BehandleOppgaveV1EndpointConfig extends AbstractCxfEndpointConfig {
 	private static final QName BEHANDLEOPPGAVE_V3_PORT_QNAME = new QName(NAMESPACE, "BehandleOppgave_v1Port");
 	private static final QName BEHANDLEOPPGAVE__V3_SERVICE_QNAME = new QName(NAMESPACE, "BehandleOppgave_v1");
 	
-	public static final String WSDL_URL = "wsdl/no/nav/tjeneste/virksomhet/behandleOppgave/v1/behandleOppgave.wsdl";
+	public static final String WSDL_URL = "wsdl/no/nav/tjeneste/virksomhet/behandleOppgave/v1/Binding.wsdl";
 	
 	@Bean
 	public BehandleOppgaveV1 behandleOppgaveV1(BehandleOppgaveV1Alias behandleOppgaveV1Alias, NavAppCertAlias navAppCertAlias) {
@@ -31,7 +32,7 @@ public class BehandleOppgaveV1EndpointConfig extends AbstractCxfEndpointConfig {
 		setReceiveTimeout(behandleOppgaveV1Alias.getReadtimeoutms());
 		setConnectTimeout(behandleOppgaveV1Alias.getConnecttimeoutms());
 		addOutInterceptor(new SystemSAMLOutInterceptor());
-//		addFeature(new WSAddressingFeature());
+		addFeature(new WSAddressingFeature());
 		return createPort(BehandleOppgaveV1.class);
 	}
 }
