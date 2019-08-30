196197917d26d032afb13fa2f63e6a20519a8b0a
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2019-08-30 13:45:38 +0200

    MMA-3459 Oppdatert fra Java 8 til Java 11.

--- app/src/main/java/no/nav/dokopp/Application.java
+++ app/src/main/java/no/nav/dokopp/Application.java
@@ -13,6 +13,10 @@ public class Application {
 		DokoppConfigSetter configSetter = new DokoppConfigSetter();
 		configSetter.configureSsl();
 		configSetter.setAppConfig();
+		// Skru av runtime bytecode optimalisering i jaxb.
+		// Denne funksjonaliteten vil bli fjernet i sin helhet i JAXB referanse implementasjonen
+		// org.glassfish.jaxb:jaxb-runtime:2.4.0 (ikke publisert pr d.d.).
+		System.setProperty("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true");
 		SpringApplication.run(Application.class, args);
 	}
 }
