5c90abe7de6dae94b294c36ef7206ef8ea06f226
Erik Braten <erik.braten@nav.no>
2019-09-19 10:28:01 +0200

    MMA-3400 PR forbedringer

--- app/src/main/java/no/nav/dokopp/qopp001/IdsProcessor.java
+++ app/src/main/java/no/nav/dokopp/qopp001/IdsProcessor.java
@@ -1,6 +1,6 @@
 package no.nav.dokopp.qopp001;
 
-import no.nav.dokopp.constants.MdcConstants;
+import no.nav.dokopp.constants.HeaderConstants;
 import no.nav.modig.common.MDCOperations;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -19,11 +19,11 @@ public class IdsProcessor implements Processor {
 	}
 
 	private void addCallIdToMdc(Exchange exchange) {
-		String callId = exchange.getIn().getHeader(MdcConstants.NAV_CALL_ID, String.class);
+		String callId = exchange.getIn().getHeader(HeaderConstants.NAV_CALL_ID, String.class);
 		if (callId == null || callId.trim().isEmpty()) {
 			callId = UUID.randomUUID().toString();
 		}
-		MDC.put(MdcConstants.NAV_CALL_ID, callId);
+		MDC.put(HeaderConstants.NAV_CALL_ID, callId);
 		MDC.put(MDCOperations.MDC_CALL_ID, callId); // ref. MDCUsernameTokenOutHandler
 	}
 }
