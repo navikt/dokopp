2e63055d221ac313a65604387be474783d943fa3
Joakim Borgersen <joakim.borgersen@nav.no>
2021-03-11 13:41:30 +0100

    MMA-5149: Ikke lage oppgave på fagområde STO

--- app/src/main/java/no/nav/dokopp/qopp001/Qopp001Service.java
+++ app/src/main/java/no/nav/dokopp/qopp001/Qopp001Service.java
@@ -29,6 +29,7 @@ import javax.inject.Inject;
 public class Qopp001Service {
 
 	private static final int ANTALL_RETUR = 1;
+	private static final String FAGOMRAADE_STO = "STO";
 	private final Oppgave oppgave;
 	private final OpprettOppgaveMapper opprettOppgaveMapper;
 	private final Tjoark122HentJournalpostInfo tjoark122HentJournalpostInfo;
@@ -55,9 +56,12 @@ public class Qopp001Service {
 		if (hentJournalpostInfoResponseTo.isAlleredeRegistrertReturpost()) {
 			throw new ReturpostAlleredeFlaggetException("qopp001 har oppdaget at returpost allerede er flagget som antallRetur=" + hentJournalpostInfoResponseTo.getAntallRetur() + ". Oppretter ikke oppgave i Gosys");
 		} else {
-			Integer oppgaveId = oppgave.opprettOppgave(opprettOppgaveMapper.map(hentJournalpostInfoResponseTo, opprettOppgave));
-			log.info("qopp001 har opprettet oppgave i Gosys med oppgaveId={}, fagområde={} for returpost med journalpostId={}.", oppgaveId, hentJournalpostInfoResponseTo.getFagomrade(), journalpostId);
-
+			if (FAGOMRAADE_STO.equals(hentJournalpostInfoResponseTo.getFagomrade())) {
+				log.info("qopp001 har ikke opprettet oppgave i Gosys da fagområde={} for returpost med journalpostId={}.", hentJournalpostInfoResponseTo.getFagomrade(), journalpostId);
+			} else {
+				Integer oppgaveId = oppgave.opprettOppgave(opprettOppgaveMapper.map(hentJournalpostInfoResponseTo, opprettOppgave));
+				log.info("qopp001 har opprettet oppgave i Gosys med oppgaveId={}, fagområde={} for returpost med journalpostId={}.", oppgaveId, hentJournalpostInfoResponseTo.getFagomrade(), journalpostId);
+			}
 			tjoark110SettJournalpostAttributter.settJournalpostAttributter(new SettJournalpostAttributterRequestTo(journalpostId, ANTALL_RETUR));
 			log.info("qopp001 har flagget journalpost med journalpostId={} som returpost.", journalpostId);
 		}
--- app/src/test/java/no/nav/dokopp/qopp001/itest/Qopp001IT.java
+++ app/src/test/java/no/nav/dokopp/qopp001/itest/Qopp001IT.java
@@ -21,7 +21,11 @@ import static org.awaitility.Awaitility.await;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import no.nav.dokopp.Application;
+import no.nav.dokopp.qopp001.Qopp001Service;
 import no.nav.modig.core.test.FileUtils;
 import no.nav.modig.testcertificates.TestCertificates;
 import org.apache.activemq.command.ActiveMQTextMessage;
@@ -34,6 +38,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
@@ -51,6 +56,7 @@ import javax.xml.bind.JAXBElement;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * @author Joakim Bjørnstad, Jbit AS
@@ -179,6 +185,29 @@ public class Qopp001IT {
 			verify(postRequestedFor(urlEqualTo("/oppgaver"))
 					.withRequestBody(matchingJsonPath("$[?(@.aktoerId == null)]"))
 					.withRequestBody(matchingJsonPath("$[?(@.orgnr == '" + ORGNR + "')]")));
+		});
+	}
+
+	@Test
+	public void shouldNotOppretteOppgaveWithFagomraadeSTO() throws Exception {
+		Logger fooLogger = (Logger) LoggerFactory.getLogger(Qopp001Service.class);
+		ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+		listAppender.start();
+		fooLogger.addAppender(listAppender);
+
+		stubFor(post("/arkiverdokumentproduksjon").willReturn(aResponse().withStatus(HttpStatus.OK.value())
+				.withBodyFile("tjoark110/tjoark110_happy.xml")));
+		stubFor(post("/dokumentproduksjoninfo").willReturn(aResponse().withStatus(HttpStatus.OK.value())
+				.withBodyFile("tjoark122/tjoark122_fagomraade_STO.xml")));
+		stubGetSecurityToken();
+
+		sendStringMessage(qopp001, classpathToString("qopp001/qopp001_happy.xml"), CALLID);
+
+		await().atMost(10, SECONDS).untilAsserted(() -> {
+			assertThat(listAppender.list.size(), is(3));
+			assertThat(listAppender.list.get(0).getFormattedMessage(), is("qopp001 har hentet journalpostInfo fra Joark for returpost med journalpostId=123456."));
+			assertThat(listAppender.list.get(1).getFormattedMessage(), is("qopp001 har ikke opprettet oppgave i Gosys da fagområde=STO for returpost med journalpostId=123456."));
+			assertThat(listAppender.list.get(2).getFormattedMessage(), is("qopp001 har flagget journalpost med journalpostId=123456 som returpost."));
 		});
 	}
 
