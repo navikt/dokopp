6d67eed467a31239c3f1da6fd7b97cbd94ba95aa
tsigab <tsigab.angosom@gmail.com>
2022-09-13 12:21:16 +0200

    MMA-6308 la til MQ TLS koblingen.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM navikt/java:17
 
 COPY app/target/app.jar /app/app.jar
 COPY export-vault-secrets.sh /init-scripts/50-export-vault-secrets.sh
-
-ENV JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom \
-               -Dspring.profiles.active=nais"
+COPY dokopp-java-opts.sh /init-scripts/50-dokopp-java-opts.sh
--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -11,3 +11,23 @@ then
     echo "Setting SERVICEUSER_PASSWORD"
     export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/srvdokopp/password)
 fi
+
+if test -f /var/run/secrets/nais.io/certificate/srvdokopp/keystore
+then
+    echo "Setting SRVDOKOPPCERT_KEYSTORE"
+    CERT_PATH='/var/run/secrets/nais.io/certificate/srvdokopp/keystore-extracted'
+    openssl base64 -d -A -in /var/run/secrets/nais.io/certificate/srvdokopp/keystore -out $CERT_PATH
+    export SRVDOKOPPCERT_KEYSTORE=$CERT_PATH
+fi
+
+if test -f /var/run/secrets/nais.io/certificate/srvdokopp/keystorealias
+then
+    echo "Setting SRVDOKOPPCERT_KEYSTOREALIAS"
+    export SRVDOKOPPCERT_KEYSTOREALIAS=$(cat /var/run/secrets/nais.io/certificate/srvdokopp/keystorealias)
+fi
+
+if test -f /var/run/secrets/nais.io/certificate/srvdokopp/keystorepassword
+then
+    echo "Setting SRVDOKOPPCERT_PASSWORD"
+    export SRVDOKOPPCERT_PASSWORD=$(cat /var/run/secrets/nais.io/certificate/srvdokopp/keystorepassword)
+fi
