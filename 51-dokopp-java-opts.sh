92f3a661b7901832c696d1d51e638f62a09ed789
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-06-03 11:50:20 +0200

    Fiks for feilende melding
    * Fikset minnebruk til 60% av memory limit
    * Erstattet spring-boot-starter-oauth2-client med spring-security-oauth2-client

--- 51-dokopp-java-opts.sh
+++ 51-dokopp-java-opts.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+JAVA_OPTS="${JAVA_OPTS} -XX:MaxRAMPercentage=60"
 JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.keyStore=${DOKOPPCERT_KEYSTORE}"
 JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.keyStoreType=jks"
 JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=nais"
